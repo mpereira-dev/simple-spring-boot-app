1e751e98fcd592e0b599fcde89c762bf635feda2
Miguel Pereira <mpereira.dev@gmail.com>
2017-06-14 15:18:21 -0400

    Switched to Spring Data JPA repository

--- src/main/java/com/boot/model/Shipwreck.java
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -1,89 +1,99 @@
 package com.boot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Shipwreck {
-	Long id;
-	String name;
-	String description;
-	String condition;
-	Integer depth;
-	Double latitude;
-	Double longitude;
-	Integer yearDiscovered;
 
-	public Shipwreck() { }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+    String description;
+    String condition;
+    Integer depth;
+    Double latitude;
+    Double longitude;
+    Integer yearDiscovered;
 
-	public Shipwreck(Long id, String name, String description, String condition, Integer depth, Double latitude, Double longitude, Integer yearDiscovered) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.condition = condition;
-		this.depth = depth;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.yearDiscovered = yearDiscovered;
-	}
+    public Shipwreck() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Shipwreck(Long id, String name, String description, String condition, Integer depth, Double latitude, Double longitude, Integer yearDiscovered) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.condition = condition;
+        this.depth = depth;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.yearDiscovered = yearDiscovered;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getCondition() {
-		return condition;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setCondition(String condition) {
-		this.condition = condition;
-	}
+    public String getCondition() {
+        return condition;
+    }
 
-	public Integer getDepth() {
-		return depth;
-	}
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
 
-	public void setDepth(Integer depth) {
-		this.depth = depth;
-	}
+    public Integer getDepth() {
+        return depth;
+    }
 
-	public Double getLatitude() {
-		return latitude;
-	}
+    public void setDepth(Integer depth) {
+        this.depth = depth;
+    }
 
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
+    public Double getLatitude() {
+        return latitude;
+    }
 
-	public Double getLongitude() {
-		return longitude;
-	}
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
 
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
+    public Double getLongitude() {
+        return longitude;
+    }
 
-	public Integer getYearDiscovered() {
-		return yearDiscovered;
-	}
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 
-	public void setYearDiscovered(Integer yearDiscovered) {
-		this.yearDiscovered = yearDiscovered;
-	}
+    public Integer getYearDiscovered() {
+        return yearDiscovered;
+    }
+
+    public void setYearDiscovered(Integer yearDiscovered) {
+        this.yearDiscovered = yearDiscovered;
+    }
 }
