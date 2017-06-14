4944eaf8df0ff22caff62ae965b05e1f83769445
Miguel Pereira <mpereira.dev@gmail.com>
2017-06-14 13:10:10 -0400

    Running app

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -1,13 +1,16 @@
 package com.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * Hello world!
- *
+ * Scan application for spring components and wires them up using autoconfiguration
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+
+    public static void main( String[] args ) {
+        // Fires up spring boot when main is run
+        SpringApplication.run(App.class, args);
     }
 }
